c4c33f5b612470b96561cb48dc8c6425ad618f6d
Yuta <yuta256dev@gmail.com>
2025-10-03 10:24:15 +0900

    refactor: 使っていない関数を削除
    safe_add関数の削除

--- crates/utils/src/lib.rs
+++ crates/utils/src/lib.rs
@@ -1,10 +1,2 @@
 pub mod term;
 pub mod vec2;
-
-pub fn safe_add(x: usize, y: isize) -> usize {
-    if y >= 0 {
-        x.saturating_add(y as usize)
-    } else {
-        x.saturating_sub((-y) as usize)
-    }
-}
