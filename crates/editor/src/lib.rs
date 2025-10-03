a93f5e2bcbe2401f7b95b1f2190a1178aa891cdc
Yuta <yuta256dev@gmail.com>
2025-10-03 13:58:53 +0900

    refactor: Edito構造体を分割
    Editor構造体をEditorApplicationとEditorStateに分割

--- crates/editor/src/lib.rs
+++ crates/editor/src/lib.rs
@@ -71,20 +71,17 @@ impl WindowManager {
 }
 
 #[derive(Debug)]
-pub struct Editor {
+pub struct EditorState {
     #[allow(dead_code)]
     buffer_manager: BufferManager,
     window_manager: WindowManager,
     active_window: WindowId,
-    input_manager: InputManager,
     mode: Arc<Mutex<Mode>>,
     command_buf: String,
     command_map: CommandMap,
-    builtin: Builtin,
-    pub is_quit: bool,
 }
 
-impl Editor {
+impl EditorState {
     pub fn new(files: Vec<String>) -> anyhow::Result<Self> {
         let mode = Arc::new(Mutex::new(Mode::Normal));
         let mut buffer_manager = BufferManager::default();
@@ -104,12 +101,9 @@ impl Editor {
             buffer_manager,
             window_manager,
             active_window: WindowId(0),
-            input_manager: InputManager::default(),
             mode,
             command_buf: String::new(),
             command_map: CommandMap::default(),
-            builtin: Builtin::default(),
-            is_quit: false,
         })
     }
 
@@ -118,107 +112,15 @@ impl Editor {
     }
 
     async fn set_mode(&mut self, mode: Mode) {
+        if let Mode::Insert(true) = mode
+            && let Some(win) = self.get_active_window()
+        {
+            let mut win = win.lock().await;
+            win.move_by(IVec2::right()).await;
+        }
+
         let mut mutex_mode = self.mode.lock().await;
         *mutex_mode = mode;
-    }
-
-    async fn on_action(&mut self, action: Action) -> anyhow::Result<bool> {
-        match action {
-            Action::Quit => return Ok(true),
-            Action::Builtin(action) => self.builtin.on_action(action).await?,
-        }
-        Ok(false)
-    }
-
-    async fn process_normal(&mut self, evt: Event) -> anyhow::Result<bool> {
-        if let Some(action) = self.input_manager.read_event_normal(evt)? {
-            self.on_action(action).await
-        } else {
-            Ok(false)
-        }
-    }
-
-    async fn process_insert(&mut self, evt: Event, is_append: bool) -> anyhow::Result<bool> {
-        if let Some(key_code) = self.input_manager.event_to_key(evt)?
-            && let Some(active_window) = self.get_active_window()
-        {
-            let mut active_window = active_window.lock().await;
-            let cursor = active_window.get_render_cursor().await;
-            match key_code {
-                KeyCode::Char(ch) => {
-                    {
-                        let active_buffer = active_window.get_buffer();
-                        let mut active_buffer = active_buffer.lock().await;
-
-                        if ch == '\n' {
-                            active_buffer.split_line(cursor);
-                        } else {
-                            active_buffer.insert_char(cursor, ch);
-                        }
-                    }
-
-                    if ch == '\n' {
-                        active_window.move_by(IVec2::new(0, 1)).await;
-                        active_window.move_to_x(0).await;
-                    } else {
-                        active_window.move_by(IVec2::right()).await;
-                    }
-                }
-                KeyCode::Backspace => {
-                    if cursor.x == 0 && cursor.y == 0 {
-                        return Ok(false);
-                    }
-
-                    let x = cursor.x;
-
-                    let line_len = {
-                        let active_buffer = active_window.get_buffer();
-                        let mut active_buffer = active_buffer.lock().await;
-
-                        let line_len = if cursor.x == 0 && cursor.y != 0 {
-                            active_buffer.get_line_length(cursor.y - 1)
-                        } else {
-                            None
-                        };
-
-                        if cursor.x == 0 {
-                            active_buffer.join_lines(cursor.y - 1);
-                        } else {
-                            active_buffer.remove_char(UVec2::new(cursor.x - 1, cursor.y));
-                        }
-                        line_len
-                    };
-
-                    if cursor.x != 0 {
-                        active_window.move_to_x(x - 1).await;
-                    } else if let Some(line_len) = line_len
-                        && cursor.y != 0
-                    {
-                        // 行頭に戻る
-                        active_window.move_by(IVec2::new(0, -1)).await;
-                        active_window.move_to_x(line_len).await;
-                    }
-                }
-                KeyCode::Delete => {
-                    let active_buffer = active_window.get_buffer();
-                    let mut active_buffer = active_buffer.lock().await;
-                    active_buffer.remove_char(cursor);
-                }
-                KeyCode::Esc => {
-                    if is_append {
-                        // TODO
-                        // active_window
-                        //     .on_action(WindowAction::Cursor(CursorAction::MoveLeft))
-                        //     .await;
-                    }
-
-                    self.set_mode(Mode::Normal).await;
-                }
-                _ => {}
-            }
-        }
-
-        Ok(false)
     }
 
     async fn set_command_to_normal_mode(&mut self) {
@@ -226,39 +128,176 @@ impl Editor {
         self.set_mode(Mode::Normal).await;
     }
 
-    async fn process_command(&mut self, evt: Event) -> anyhow::Result<bool> {
-        if let Some(key_code) = self.input_manager.event_to_key(evt)? {
-            match key_code {
-                KeyCode::Esc => {
-                    self.command_buf = String::new();
+    pub async fn process_command(&mut self, key_code: KeyCode) -> anyhow::Result<Option<Action>> {
+        match key_code {
+            KeyCode::Esc => {
+                self.command_buf = String::new();
+                self.set_command_to_normal_mode().await;
+            }
+            KeyCode::Backspace => {
+                if self.command_buf.is_empty() {
+                    self.set_command_to_normal_mode().await;
+                } else {
+                    self.command_buf.pop();
+                }
+            }
+            KeyCode::Char('\n') => {
+                if let Some(action) = self.command_map.get(&self.command_buf) {
+                    let action = action.clone();
+                    self.set_command_to_normal_mode().await;
+                    return Ok(Some(action));
+                } else {
                     self.set_command_to_normal_mode().await;
                 }
-                KeyCode::Backspace => {
-                    if self.command_buf.is_empty() {
-                        self.set_command_to_normal_mode().await;
-                    } else {
-                        self.command_buf.pop();
-                    }
-                }
-                KeyCode::Char('\n') => {
-                    if let Some(action) = self.command_map.get(&self.command_buf) {
-                        let is_quit = self.on_action(action.clone()).await?;
-                        self.set_command_to_normal_mode().await;
-                        return Ok(is_quit);
-                    } else {
-                        self.set_command_to_normal_mode().await;
-                    }
-                }
-                KeyCode::Char(ch) => self.command_buf.push(ch),
-                _ => {}
             }
+            KeyCode::Char(ch) => self.command_buf.push(ch),
+            _ => {}
         }
 
-        Ok(false)
+        Ok(None)
+    }
+}
+
+#[derive(Debug)]
+pub struct EditorApplication {
+    state: Arc<Mutex<EditorState>>,
+    input_manager: InputManager,
+    builtin: Builtin,
+    is_quit: bool,
+}
+
+impl EditorApplication {
+    pub fn new(files: Vec<String>) -> anyhow::Result<Self> {
+        Ok(Self {
+            state: Arc::new(Mutex::new(EditorState::new(files)?)),
+            input_manager: InputManager::default(),
+            builtin: Builtin::default(),
+            is_quit: false,
+        })
     }
 
-    async fn process(&mut self, evt: Event) -> anyhow::Result<bool> {
-        let mode = self.mode.lock().await.clone();
+    fn quit(&mut self) {
+        self.is_quit = true;
+    }
+
+    pub fn get_quit(&self) -> bool {
+        self.is_quit
+    }
+
+    async fn on_action(&mut self, action: Action) -> anyhow::Result<()> {
+        match action {
+            Action::Quit => self.quit(),
+            Action::Builtin(action) => self.builtin.on_action(action).await?,
+        }
+        Ok(())
+    }
+
+    async fn process_normal(&mut self, evt: Event) -> anyhow::Result<()> {
+        if let Some(action) = self.input_manager.read_event_normal(evt)? {
+            self.on_action(action).await?;
+        }
+        Ok(())
+    }
+
+    async fn process_insert(&mut self, evt: Event, is_append: bool) -> anyhow::Result<()> {
+        if let Some(key_code) = self.input_manager.event_to_key(evt)? {
+            let mut state = self.state.lock().await;
+            if let Some(active_window) = state.get_active_window() {
+                let mut active_window = active_window.lock().await;
+                let cursor = active_window.get_render_cursor().await;
+                match key_code {
+                    KeyCode::Char(ch) => {
+                        {
+                            let active_buffer = active_window.get_buffer();
+                            let mut active_buffer = active_buffer.lock().await;
+
+                            if ch == '\n' {
+                                active_buffer.split_line(cursor);
+                            } else {
+                                active_buffer.insert_char(cursor, ch);
+                            }
+                        }
+
+                        if ch == '\n' {
+                            active_window.move_by(IVec2::new(0, 1)).await;
+                            active_window.move_to_x(0).await;
+                        } else {
+                            active_window.move_by(IVec2::right()).await;
+                        }
+                    }
+                    KeyCode::Backspace => {
+                        if cursor.x == 0 && cursor.y == 0 {}
+
+                        let x = cursor.x;
+
+                        let line_len = {
+                            let active_buffer = active_window.get_buffer();
+                            let mut active_buffer = active_buffer.lock().await;
+
+                            let line_len = if cursor.x == 0 && cursor.y != 0 {
+                                active_buffer.get_line_length(cursor.y - 1)
+                            } else {
+                                None
+                            };
+
+                            if cursor.x == 0 {
+                                active_buffer.join_lines(cursor.y - 1);
+                            } else {
+                                active_buffer.remove_char(UVec2::new(cursor.x - 1, cursor.y));
+                            }
+                            line_len
+                        };
+
+                        if cursor.x != 0 {
+                            active_window.move_to_x(x - 1).await;
+                        } else if let Some(line_len) = line_len
+                            && cursor.y != 0
+                        {
+                            // 行頭に戻る
+                            active_window.move_by(IVec2::new(0, -1)).await;
+                            active_window.move_to_x(line_len).await;
+                        }
+                    }
+                    KeyCode::Delete => {
+                        let active_buffer = active_window.get_buffer();
+                        let mut active_buffer = active_buffer.lock().await;
+                        active_buffer.remove_char(cursor);
+                    }
+                    KeyCode::Esc => {
+                        if is_append {
+                            active_window.move_by(IVec2::left()).await;
+                        }
+
+                        state.set_mode(Mode::Normal).await;
+                    }
+                    _ => {}
+                }
+            }
+        }
+        Ok(())
+    }
+
+    async fn process_command(&mut self, evt: Event) -> anyhow::Result<()> {
+        if let Some(key_code) = self.input_manager.event_to_key(evt)? {
+            let action = {
+                let mut state = self.state.lock().await;
+                state.process_command(key_code).await?
+            };
+
+            if let Some(action) = action {
+                self.on_action(action).await?;
+            }
+        }
+        Ok(())
+    }
+
+    async fn process(&mut self, evt: Event) -> anyhow::Result<()> {
+        let mode = {
+            let state = self.state.lock().await;
+            let mode = state.mode.lock().await;
+            mode.clone()
+        };
+
         match mode {
             Mode::Normal => self.process_normal(evt).await,
             Mode::Insert(is_append) => self.process_insert(evt, is_append).await,
@@ -267,9 +306,11 @@ impl Editor {
     }
 
     async fn process_request(&mut self, request: ApiRequest) -> Option<ApiResponse> {
+        let mut state = self.state.lock().await;
+
         match request {
             ApiRequest::ChangeMode(mode) => {
-                self.set_mode(mode).await;
+                state.set_mode(mode).await;
                 Some(ApiResponse::None)
             }
             // ApiRequest::OpenFile(path) => {
@@ -280,7 +321,7 @@ impl Editor {
                 let id = if let Some(buf) = buf {
                     Some(buf)
                 } else {
-                    if let Some(active) = self.get_active_window() {
+                    if let Some(active) = state.get_active_window() {
                         let win = active.lock().await;
                         Some(win.get_buf())
                     } else {
@@ -289,7 +330,7 @@ impl Editor {
                 };
 
                 if let Some(id) = id
-                    && let Some(buf) = self.buffer_manager.get_buffer(id)
+                    && let Some(buf) = state.buffer_manager.get_buffer(id)
                 {
                     let mut buf = buf.lock().await;
                     buf.save().ok()?;
@@ -347,9 +388,9 @@ impl Editor {
             // }
             ApiRequest::MoveBy(win, offset) => {
                 let win = if let Some(win) = win {
-                    self.window_manager.get_window(win)
+                    state.window_manager.get_window(win)
                 } else {
-                    self.get_active_window()
+                    state.get_active_window()
                 };
 
                 if let Some(win) = win {
@@ -361,9 +402,9 @@ impl Editor {
             }
             ApiRequest::MoveToX(win, pos) => {
                 let win = if let Some(win) = win {
-                    self.window_manager.get_window(win)
+                    state.window_manager.get_window(win)
                 } else {
-                    self.get_active_window()
+                    state.get_active_window()
                 };
 
                 if let Some(win) = win {
@@ -380,9 +421,9 @@ impl Editor {
             }
             ApiRequest::MoveToY(win, pos) => {
                 let win = if let Some(win) = win {
-                    self.window_manager.get_window(win)
+                    state.window_manager.get_window(win)
                 } else {
-                    self.get_active_window()
+                    state.get_active_window()
                 };
 
                 if let Some(win) = win {
@@ -416,11 +457,7 @@ impl Editor {
 
     pub async fn run(&mut self, event: Event) {
         match self.process(event).await {
-            Ok(is_quit) => {
-                if is_quit {
-                    self.is_quit = true;
-                }
-            }
+            Ok(_) => {}
             Err(e) => {
                 log::error!("Error: {:?}", e);
             }
@@ -429,7 +466,7 @@ impl Editor {
 }
 
 pub async fn main(files: Vec<String>) -> anyhow::Result<()> {
-    let editor = Arc::new(Mutex::new(Editor::new(files)?));
+    let editor = Arc::new(Mutex::new(EditorApplication::new(files)?));
 
     let editor1 = editor.clone();
     let handle = tokio::spawn(async move {
@@ -437,13 +474,14 @@ pub async fn main(files: Vec<String>) -> anyhow::Result<()> {
         renderer.init_screen().ok();
         loop {
             let mut editor = editor1.lock().await;
-            if editor.is_quit {
+            if editor.get_quit() {
                 break;
             }
 
             editor.run_request().await;
 
-            let active_window = editor.get_active_window();
+            let state = editor.state.lock().await;
+            let active_window = state.get_active_window();
             if let Some(win) = active_window {
                 let buf = {
                     let win = win.lock().await;
@@ -451,7 +489,7 @@ pub async fn main(files: Vec<String>) -> anyhow::Result<()> {
                 };
 
                 renderer
-                    .render(win, buf, editor.mode.clone(), editor.command_buf.clone())
+                    .render(win, buf, state.mode.clone(), state.command_buf.clone())
                     .await
                     .unwrap();
             }
