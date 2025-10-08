66e44db26340ee8e06423c79aad3cf995331b042
Yuta <yuta256dev@gmail.com>
2025-10-08 15:18:54 +0900

    refactor: 描画処理をリファクタリング

--- src/ui/render.rs
+++ src/ui/render.rs
@@ -1,5 +1,5 @@
 use std::{
-    io::{Write, stdout},
+    io::{Stdout, Write, stdout},
     sync::Arc,
 };
 
@@ -7,7 +7,7 @@ use api::Mode;
 use crossterm::{
     cursor::{self, MoveTo},
     execute, queue,
-    style::{self, Color, Print, ResetColor, SetBackgroundColor},
+    style::{self, Color, Print, ResetColor, SetBackgroundColor, SetForegroundColor},
     terminal::{self, disable_raw_mode, enable_raw_mode},
 };
 use tokio::sync::Mutex;
@@ -35,6 +35,77 @@ impl Renderer {
         Ok(())
     }
 
+    fn render_move_cursor(&self, stdout: &mut Stdout, cursor: UVec2) -> anyhow::Result<()> {
+        queue!(stdout, MoveTo(cursor.x as u16, cursor.y as u16))?;
+        Ok(())
+    }
+
+    fn render_code_token(
+        &self,
+        stdout: &mut Stdout,
+        token: &str,
+        fg: Option<Color>,
+        bg: Option<Color>,
+    ) -> anyhow::Result<()> {
+        queue!(stdout, ResetColor)?;
+
+        if let Some(fg) = fg {
+            queue!(stdout, SetForegroundColor(fg))?;
+        }
+
+        if let Some(bg) = bg {
+            queue!(stdout, SetBackgroundColor(bg))?;
+        }
+
+        queue!(stdout, Print(token))?;
+
+        Ok(())
+    }
+
+    fn render_code_line(
+        &self,
+        stdout: &mut Stdout,
+        line: &str,
+        line_y: usize,
+        y: usize,
+        mode: &Mode,
+        visual_cursor: (UVec2, UVec2),
+        position: UVec2,
+    ) -> anyhow::Result<()> {
+        self.render_move_cursor(stdout, UVec2::new(position.x, position.y + y))?;
+
+        if let Mode::Visual = mode {
+            let (left, right) = visual_cursor;
+
+            if left.y == line_y && right.y == line_y {
+                let (line_left, line_right) = line.split_at(left.x);
+                let (line_center, line_right) = line_right.split_at(right.x - left.x);
+
+                self.render_code_token(stdout, line_left, None, None)?;
+                self.render_code_token(stdout, line_center, None, Some(Color::Blue))?;
+                self.render_code_token(stdout, line_right, None, None)?;
+            } else if left.y == line_y && !line.is_empty() {
+                let (line_left, line_right) = line.split_at(left.x);
+
+                self.render_code_token(stdout, line_left, None, None)?;
+                self.render_code_token(stdout, line_right, None, Some(Color::Blue))?;
+            } else if right.y == line_y {
+                let (line_left, line_right) = line.split_at(right.x);
+
+                self.render_code_token(stdout, line_left, None, Some(Color::Blue))?;
+                self.render_code_token(stdout, line_right, None, None)?;
+            } else if left.y < line_y && right.y > line_y {
+                self.render_code_token(stdout, line, None, Some(Color::Blue))?;
+            } else {
+                self.render_code_token(stdout, line, None, None)?;
+            }
+        } else {
+            self.render_code_token(stdout, line, None, None)?;
+        }
+
+        Ok(())
+    }
+
     pub async fn render(
         &self,
         active_window: Arc<Mutex<Window>>,
@@ -51,7 +122,7 @@ impl Renderer {
         let visual_start = win.get_visual_start().await;
         let scroll = win.get_scroll();
 
-        let (left, right) = if cursor < visual_start {
+        let visual_cursor = if cursor < visual_start {
             (cursor + UVec2::new(1, 0), visual_start + UVec2::new(1, 0))
         } else {
             (visual_start, cursor)
@@ -60,7 +131,13 @@ impl Renderer {
         let position = win.get_position();
         let size = win.get_size();
 
-        queue!(stdout(), cursor::MoveTo(0, 0))?;
+        let mut stdout = stdout();
+
+        queue!(
+            stdout,
+            cursor::MoveTo(0, 0),
+            terminal::Clear(terminal::ClearType::All)
+        )?;
 
         let mode = mode.lock().await.clone();
         let buf = active_buffer.lock().await;
@@ -71,83 +148,29 @@ impl Renderer {
             .take(size.y)
             .enumerate()
         {
-            let line_y = scroll + y;
-            if let Mode::Visual = mode {
-                if left.y == line_y && right.y == line_y {
-                    let (line_left, line_right) = line.split_at(left.x);
-                    let (line_center, line_right) = line_right.split_at(right.x - left.x);
-                    queue!(
-                        stdout(),
-                        MoveTo(position.x as u16, (position.y + y) as u16),
-                        Print(line_left),
-                        SetBackgroundColor(Color::Blue),
-                        Print(line_center),
-                        ResetColor,
-                        Print(line_right),
-                        Print(" ".repeat(size.x - line.len()))
-                    )?;
-                } else if left.y == line_y && !line.is_empty() {
-                    let (line_left, line_right) = line.split_at(left.x);
-                    queue!(
-                        stdout(),
-                        MoveTo(position.x as u16, (position.y + y) as u16),
-                        Print(line_left),
-                        SetBackgroundColor(Color::Blue),
-                        Print(line_right),
-                        ResetColor,
-                        Print(" ".repeat(size.x - line.len()))
-                    )?;
-                } else if right.y == line_y {
-                    let (line_left, line_right) = line.split_at(right.x);
-                    queue!(
-                        stdout(),
-                        MoveTo(position.x as u16, (position.y + y) as u16),
-                        SetBackgroundColor(Color::Blue),
-                        Print(line_left),
-                        ResetColor,
-                        Print(line_right),
-                        Print(" ".repeat(size.x - line.len()))
-                    )?;
-                } else if left.y < line_y && right.y > line_y {
-                    queue!(
-                        stdout(),
-                        MoveTo(position.x as u16, (position.y + y) as u16),
-                        SetBackgroundColor(Color::Blue),
-                        Print(line),
-                        ResetColor,
-                        Print(" ".repeat(size.x - line.len()))
-                    )?;
-                } else {
-                    queue!(
-                        stdout(),
-                        MoveTo(position.x as u16, (position.y + y) as u16),
-                        Print(line),
-                        Print(" ".repeat(size.x - line.len()))
-                    )?;
-                }
-            } else {
-                queue!(
-                    stdout(),
-                    MoveTo(position.x as u16, (position.y + y) as u16),
-                    Print(line),
-                    Print(" ".repeat(size.x - line.len()))
-                )?;
-            }
+            self.render_code_line(
+                &mut stdout,
+                line,
+                y + scroll,
+                y,
+                &mode,
+                visual_cursor,
+                position,
+            )?;
         }
 
         if let Mode::Command = mode {
             queue!(
-                stdout(),
+                stdout,
                 cursor::MoveTo(0, h - 1),
                 Print(':'),
                 Print(&command_buf),
-                Print(" ".repeat(w as usize - command_buf.len() - 1))
             )?;
         } else {
             let status = format!(" {} ", mode);
 
             queue!(
-                stdout(),
+                stdout,
                 cursor::MoveTo(0, h - 1),
                 style::SetBackgroundColor(style::Color::White),
                 style::SetForegroundColor(style::Color::Black),
@@ -158,7 +181,7 @@ impl Renderer {
 
             let cursor = UVec2::new(cursor.x, cursor.y.saturating_sub(scroll));
             queue!(
-                stdout(),
+                stdout,
                 cursor::MoveTo(
                     (position.x + cursor.x) as u16,
                     (position.y + cursor.y) as u16
@@ -167,12 +190,12 @@ impl Renderer {
         }
 
         if let Mode::Normal | Mode::Visual = mode {
-            queue!(stdout(), cursor::SetCursorStyle::SteadyBlock)?;
+            queue!(stdout, cursor::SetCursorStyle::SteadyBlock)?;
         } else {
-            queue!(stdout(), cursor::SetCursorStyle::SteadyBar)?;
+            queue!(stdout, cursor::SetCursorStyle::SteadyBar)?;
         }
 
-        stdout().flush()?;
+        stdout.flush()?;
 
         Ok(())
     }
